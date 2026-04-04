14fff0534eec1be804010250b1add2f7f1abadc5
Aham0803 <ahamgolash9340@gmail.com>
2026-04-04 18:36:33 +0530

    Add files via upload

---
+++ UpdateSyntax.sql
@@ -0,0 +1,10 @@
+/* syntax 
+update ->
+Update table_name
+Set column1 = value1,
+    column2 = value 2
+where <condition>
+
+Note -> Always use where to avoid updating all rows unintensionally
+*/
+
