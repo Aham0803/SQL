fb9616ac762a2ec56b3a9f06779f1d7d48c0b0ac
Aham0803 <ahamgolash9340@gmail.com>
2026-04-04 18:20:42 +0530

    Add files via upload

---
+++ InsertValues.sql
@@ -0,0 +1,9 @@
+Insert into customers(id , first_name , country , score)
+values 
+   (6 , 'aham' , 'india' , 999),
+    (7 , 'sheetal' , 'india' , 1000),
+    (8 , 'sam' , 'germany' , 100),
+    (9 , 'andreas' , 'USA' , 200),
+    (10 , 'sahra' , null , null)
+Select*
+from customers
