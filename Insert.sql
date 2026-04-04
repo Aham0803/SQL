49775a32ec86b67a421c6fcce0f4162fbb7644fc
Aham0803 <ahamgolash9340@gmail.com>
2026-04-04 18:00:56 +0530

    Add files via upload

---
+++ Insert.sql
@@ -0,0 +1,8 @@
+/*
+Syntax ->
+Insert Into table_name (column1 , column2 , column3 , ...)
+Values (value1 , value2 , value3....) 
+, (value1 , value2 , value3...)
+
+Note -> Match the number of columns and values.
+ */
